a065d4b925c925f24d5665522cb6b1d09e9fabaf
spasztor <szabolcs1992@gmail.com>
2015-12-15 18:42:47 -0800

    Updated profile to make it work everywhere.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,33 +1,32 @@
 # Aliases for vim.
-Set-Alias vi "C:\Program Files (x86)\Vim\vim74\vim.exe"
-Set-Alias gvi "C:\Program Files (x86)\Vim\vim74\gvim.exe"
+Set-Alias vim "C:\Program Files (x86)\Vim\vim74\vim.exe"
+Set-Alias gvim "C:\Program Files (x86)\Vim\vim74\gvim.exe"
 
 # For editing your PowerShell profile
 Function Edit-Profile
 {
-    vi $profile
+    vim $profile
 }
 
 # For editing your Vim settings
 Set-Variable VIMRC $home\_Vimrc
 Function Edit-Vimrc
 {
-    vi $home\_vimrc
+    vim $home\_vimrc
 }
-
-# Quick Alias for working folder.
-Set-Variable WORK "K:\common\Szabolcs_Pasztor\Programming"
-cd $WORK
 
 # Strange magical environment variables to get vim working properly.
 $env:vim = "C:\Program Files (x86)\Vim\vim74"
 $env:gvim = "C:\Program Files (x86)\Vim\vim74"
 
-# Ti get git loading properly, be sure to follow instructions here:
+# To get git loading properly, be sure to follow instructions here:
 # http://learnaholic.me/2012/10/12/make-powershell-and-git-suck-less-on-windows/
-#
-# Load posh-git example profile
-. 'K:\common\Szabolcs_Pasztor\Utilities\Developing\_cloned\posh-git\profile.example.ps1'
+
+# For getting posh-git & ssh-agent to work:
+$env:path += ";" + (Get-Item "Env:ProgramFiles(x86)").Value + "\Git\bin"
+
+# Load posh-git example profile:
+. "C:\Users\Szabi\Documents\WindowsPowerShell\Modules\posh-git\profile.example.ps1"
 
 # Script for longer history in shell:
 $HistoryFilePath = Join-Path ([Environment]::GetFolderPath('UserProfile')) .ps_history
@@ -37,5 +36,3 @@ if (Test-path $HistoryFilePath) { Import-Clixml $HistoryFilePath | Add-History }
 # if you don't already have this configured...
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
-
-
